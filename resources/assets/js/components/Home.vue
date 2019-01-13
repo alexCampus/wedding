341fbb47c1073020584a07ac93b083e2ae20ee6a
Alexandre De-Pembroke <alex.depem@gmail.com>
2019-01-13 19:01:31 +0100

    mise a jour github avec component vue

---
+++ resources/assets/js/components/Home.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Example Component</div>
+
+                    <div class="panel-body">
+                        <label>
+                            <select v-model="selected">
+                                <option disabled value="">Choisissez</option>
+                                <option>Test 1</option>
+                                <option>Test 4</option>
+                                <option>Test 9</option>
+                                <option>Test 5</option>
+                                <option>Test 3</option>
+                            </select>
+                        </label>
+                        <p>{{selected}}</p>
+                        I'm an example vuygjgg
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                selected: '',
+            };
+        },
+
+        mounted() {
+            console.log('Component mounted.');
+        },
+    };
+</script>
---
+++ resources/assets/js/components/Subcriber.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: 'Subcriber',
+    };
+</script>
+
+<style scoped>
+
+</style>
